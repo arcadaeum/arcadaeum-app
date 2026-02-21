5e2e2e44bda827770eb5a188ad3df88622a2bb56
Fred Stonell <stonellfred@gmail.com>
2026-02-21 00:18:22 +0000

    added logo to nav bar and edited user page

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import smallLogo from "../assets/images/small_logo.jpg";
 
 export default function NavigationBar() {
 	return (
-		<nav className="absolute top-4 right-4 z-20 space-x-4">
-			<Link to="/">Home</Link>
-			<Link to="/signin">Sign In</Link>
-			<Link to="/user">User Page</Link>
+		<nav className="absolute top-4 left-4 right-4 z-20 font-main flex justify-between items-center">
+			{/* Logo on top-left */}
+			<Link to="/">
+				<img src={smallLogo} alt="Arcadaeum Logo" className="h-15 w-15" />
+			</Link>
+
+			{/* Right-side links */}
+			<div className="space-x-4 text-base">
+				<Link to="/signin">Sign In</Link>
+				<Link to="/user">User Page</Link>
+			</div>
 		</nav>
 	);
 }
--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -3,8 +3,14 @@ import NavigationBar from "../components/NavigationBar";
 function UserPage() {
 	return (
 		<>
-			<h1>This is a user page! woah</h1>
 			<NavigationBar />
+			<div className="flex flex-col items-center font-main min-h-screen pt-20">
+				<div className="w-30 h-30 bg-white rounded-full"></div>
+				<h1 className="mt-8 text-3xl font-secondary text-center">username_here</h1>
+				<h2 className="w-full max-w-4xl mt-12 text-lg font-secondary text-gray-300 border-b-4 border-arcade-gold">
+					FAVORITE GAMES
+				</h2>
+			</div>
 		</>
 	);
 }
