d884803b18d904cd1b609c10ad42faca8023c4d1
Archie Sagers <archie.sagers1@gmail.com>
2026-03-06 13:03:02 +0000

    Made Sign-In page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import UserPage from "./pages/UserPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
 
 export default function App() {
 	return (
@@ -9,6 +10,7 @@ export default function App() {
 			<Route path="/" element={<HomePage />} />
 			<Route path="/signin" element={<SignInPage />} />
 			<Route path="/user" element={<UserPage />} />
+			<Route path="/CreateAccount" element={<CreateAccountPage />} />
 		</Routes>
 	);
 }
