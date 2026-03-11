81927f58f0be45e7e010db42a9afb947f7009f8b
Elliott Feltham <elliottfeltham98@gmail.com>
2026-03-11 16:48:32 +0000

    Successfully adds games to database on startup
    Allows for safe overriding of games in database once when the caching is called again.

---
+++ backend/app/services/cache.py
@@ -0,0 +1,51 @@
+from app.services.igdb_service import IGDBService
+from app.database import add_game_to_db
+
+igdb = IGDBService()
+
+
+def cache_popular_games(limit: int = 500):
+    try:
+        # Fetch From Service
+        games_data = igdb.fetch_top_games(limit=limit)
+
+        if not games_data:
+            return {"message": "No games fetched from IGDB"}
+
+        # Logic to save to our DB
+        saved_count = 0
+        for game_data in games_data:
+            try:
+                cover_url = None
+                cover = game_data.get("cover")
+                if isinstance(cover, dict) and cover.get("image_id"):
+                    image_id = cover["image_id"]
+
+                    # Change size by changing 't_cover_big' to other size options, check IGDB docs for more: https://api-docs.igdb.com/#images
+                    cover_url = (
+                        f"https://images.igdb.com/igdb/image/upload/t_cover_big/{image_id}.jpg"
+                    )
+
+                platform_names = []
+                for platform in game_data.get("platforms", []):
+                    if isinstance(platform, dict) and platform.get("name"):
+                        platform_names.append(platform["name"])
+
+                add_game_to_db(
+                    igdb_id=game_data.get("id"),
+                    title=game_data.get("name"),
+                    summary=game_data.get("summary"),
+                    cover_url=cover_url,
+                    platforms=platform_names,
+                    release_date=game_data.get("first_release_date"),
+                    igdb_rating=game_data.get("total_rating"),
+                )
+                saved_count += 1
+            except Exception as error:
+                print(f"Error saving game {game_data.get('name')}: {error}")
+                continue
+
+        return {"message": f"Successfully cached {saved_count} games"}
+    except Exception as error:
+        print(f"Cache error: {error}")
+        return {"error": str(error)}
